f48c879d5581d9b0b1777a488f77f41ca12e2506
antonybaby <antony.baby@gmail.com>
2021-05-18 10:08:56 -0400

    modified version May 18

--- Test2.py
+++ Test2.py
@@ -1,2 +1,6 @@
 print("Hello World #############")
+<<<<<<< HEAD
 print("added second line of comment $$$$$$$$$$$$$$$$$$$$$")
+=======
+## print("added second line of comment $$$$$$$$$$$$$$$$$$$$$")
+>>>>>>> acca4da (modified version May 18)
